e60600d23ddc1293564d347cf5392efc57f301cc
Corrod3 <ohne.realitaet@posteo.de>
2016-11-04 17:28:01 +0100

    Control variable cleaning UR and PD

--- control/getControl.R
+++ control/getControl.R
@@ -4,12 +4,51 @@
 # Torben&Alex
 ###########################
 
-# 0. Preparation
-# 1. Import Data
-# 2. Clean data
+# 0. Notes
+# 1. Preparation
+# 2. Import Data
+# 3. Clean data
+# 4. Notes
+
+##########################
+# 0. Notes
+##########################
+
+# ToDO
+# 1. Population density 
+#     Source: Destatis: "Bevölkerungsdichte: Bundesländer, Stichtag"
+#     Get Data: Done
+#     Clean Data: Done
+# 2. Unemployment rate
+#     Source: Destatis: "Arbeitslose, Arbeitslosenquoten, Gemeldete Arbeitsstellen: Bundesländer, Jahre"
+#     Get Data: Done
+#     Clean Data: Done
+# 3. youth unemployment -
+#     Source: Regionaldatenbank? 
+#             difficult to find, but exists:
+#             e.g. behind paywall: https://de.statista.com/statistik/daten/studie/36739/umfrage/jugendarbeitslosigkeit-nach-bundeslaendern/
+#             or for single states: https://www.govdata.de/web/guest/daten/-/details/stala-sn-service-13211-004z
+#             https://www.statistik.sachsen-anhalt.de/apps/StrukturKompass/indikator/zeitreihe/90
+#     Get Data: 
+#     Clean Data: 
+# 4. state gdp
+#     Source: Destatis: "VGR der Länder - Bruttoinlandsprodukt"
+#             http://www.vgrdl.de/VGRdL/tbls/tab.jsp?rev=RV2014&tbl=tab01&lang=de-DE
+#     Get Data: 
+#     Clean Data:
+# 5. Bildungsniveau
+#     Source:
+#     Get Data: 
+#     Clean Data: 
+# 6. Alcohol sales / Beer sales
+#     Source: https://www.destatis.de/GPStatistik/receive/DEHeft_heft_00014241
+#             https://www.destatis.de/GPStatistik/receive/DESerie_serie_00000146?list=all
+#     Get Data: 
+#     Clean Data: 
+# 7. Restructure Script: by variable instead of work step    
 
 ###########################
-# 0. Preparations
+# 1. Preparations
 ###########################
 
 # Clear Global environment
@@ -20,7 +59,7 @@ try(setwd("/home/torben/GIT/Pair_Assignment_2"), silent = TRUE)
 try(setwd("D:/Eigene Datein/Dokumente/Uni/Hertie/Materials/Collaborative Social Science Data Analysis/CSSR_Project"), silent = TRUE)
 
 # Collect packages/libraries we need:
-packages <- c("readxl", "RCurl", "ckanr", "plyr")
+packages <- c("readxl", "RCurl", "ckanr", "plyr","reshape")
 
 # install packages if not installed before
 for (p in packages) {
@@ -35,7 +74,7 @@ for (p in packages) {
 rm(p, packages)
 
 ###########################
-# 1. Import data
+# 2. Import data
 ###########################
 
 # Download population density if not in directory
@@ -53,10 +92,7 @@ if(class(try(read.csv("control/UR.csv")))=="try-error") {
 UR <- read.csv("control/UR.csv")
 
 # youth unemployment rates
-#difficult to find, but exists:
-# e.g. behind paywall: https://de.statista.com/statistik/daten/studie/36739/umfrage/jugendarbeitslosigkeit-nach-bundeslaendern/
-# or for single states: https://www.govdata.de/web/guest/daten/-/details/stala-sn-service-13211-004z
-# https://www.statistik.sachsen-anhalt.de/apps/StrukturKompass/indikator/zeitreihe/90
+
 
 # IDEA: Use CKAN access of govdata.de
 # https://cran.r-project.org/web/packages/ckanr/ckanr.pdf
@@ -66,24 +102,52 @@ UR <- read.csv("control/UR.csv")
 # set url and key
 #ckanr_setup(url = "http://data.techno-science.ca/", key = "my-ckan-api-key")
 
-
-
 ###########################
-# 2. Clean data
+# 3. Clean data
 ###########################
 
 #Cleaning and Renaming PD
 #ISO 3166-2:DE
 PD <- PD[-c(1:6, 27:31),-c(1)]
-colnames(PD) <- c("year", "DE-BW", "DE-BY", "DE-BE", "DE-BB", "DE-HB", "DE-HH", "DE-HE", "DE-MV", 
-             "DE-NI", "DE-NW", "DE-RP", "DE-SL", "DE-SN", "DE-ST", "DE-SH", "DE-TH")
+colnames(PD) <- c("year", "DE-BW", "DE-BY", "DE-BE", "DE-BB", "DE-HB", "DE-HH",
+                  "DE-HE", "DE-MV", "DE-NI", "DE-NW", "DE-RP", "DE-SL", 
+                  "DE-SN", "DE-ST", "DE-SH", "DE-TH")
 rownames(PD) <- c(1995:2014)
 PD[,1] <- c(1995:2014)
+PD <- melt(PD, id.vars = "year")
+colnames(PD) <- c("YEAR", "STATE", "POD")
 
+# Delete last rows with stats sources etc.
+UR <- UR[-c(1:6,407:421),-1]
 
+# Rename columns
+colnames(UR) <- c("STATE","YEAR", "UTOTAL", "UR", "URD" ,"VACTOTAL")
 
+# Delete leading and tailing whitespace
+UR <- as.data.frame(lapply(UR, function(x) trimws(x)), 
+                      stringsAsFactors = FALSE)
 
+# Recode empty cells
+UR[UR == "-"] <- NA
 
+# Recode STATE and make it factor
+UR$STATE <- mapvalues(as.matrix(UR$STATE), 
+                        c("Baden-Württemberg", "Bayern", "Berlin", "Brandenburg", 
+                          "Bremen", "Hamburg", "Hessen", 
+                          "Mecklenburg-Vorpommern", "Niedersachsen", 
+                          "Nordrhein-Westfalen", "Rheinland-Pfalz", "Saarland", 
+                          "Sachsen", "Sachsen-Anhalt", "Schleswig-Holstein", 
+                          "Thüringen"), 
+                        c("DE-BW", "DE-BY", "DE-BE", "DE-BB", "DE-HB", "DE-HH", 
+                          "DE-HE", "DE-MV", "DE-NI", "DE-NW", "DE-RP", "DE-SL", 
+                          "DE-SN", "DE-ST", "DE-SH", "DE-TH"))
+UR$STATE <- as.factor(UR$STATE)
+UR$STATE <- factor(UR$STATE, levels = c("DE-BW", "DE-BY", "DE-BE",
+                                            "DE-BB", "DE-HB", "DE-HH", 
+                                            "DE-HE", "DE-MV", "DE-NI", 
+                                            "DE-NW", "DE-RP", "DE-SL", 
+                                            "DE-SN", "DE-ST", "DE-SH", 
+                                            "DE-TH"))
 
 
 
