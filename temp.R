954354fb5aebf83b5f11526ec0dc0a14823eaea2
Corrod3 <ohne.realitaet@posteo.de>
2016-12-01 17:56:21 +0100

    update

--- temp.R
+++ temp.R
@@ -14,7 +14,8 @@ Z1.2000 <- zelig(F100_p1000 ~ GDP_P_C + UR.LF + PD, cite = FALSE,
 # Simulation of relationhip F10.0 diagnoses and GDP per capita
 setZ.GDP.PP <- setx(Z1.2000, GDP_P_C = 15:46)
 simZ.GDP.PP <- sim(Z1.2000, x = setZ.GDP.PP)
-ci.plot(simZ.GDP.PP)
+ci.plot(simZ.GDP.PP, ylab = "Conditional Exp. Change F10.0 Diagnoses per 1000", xlab = "")
+title(xlab="Change of GDP", line=-1, cex.lab=1)
 
 # Simulation of relationhip F10.0 diagnoses and unemployment rate
 setZ.UR.LF <- setx(Z1.2000, UR.LF = 5:21)
@@ -33,4 +34,4 @@ ci.plot(simZ2.GDP.D)
 # Simulation of relationhip F10.0 diagnoses and GDP per capita
 setZ2.UR.LF.D <- setx(Z2.F100, UR.LF.D = -3:4)
 simZ2.UR.LF.D <- sim(Z2.F100, x = setZ2.UR.LF.D)
-ci.plot(simZ2.UR.LF.D)
+ci.plot(simZ2.UR.LF.D, ylab = "Conditional Exp. Change F10.0 Diagnoses per 1000", xlab = "Change of unemployment")
